aa5e4eb2fd7a8ef016fca50b49c779cae62e18f3
pengwius <pengwius@protonmail.ch>
2026-02-01 20:00:57 +0100

    remove unused conditionals from index

--- src/data/index.js
+++ src/data/index.js
@@ -1,40 +1,40 @@
-import { tenses } from './tenses';
-import { conditionals } from './conditionals';
-import { przyimki } from './przyimki';
-import {thereIsItIsData} from "./thereIs"
-import { przymiotniki } from './przymiotniki';
-import {reportedSpeechData} from "./mowaZalezna"
-import {verbPatternsData} from "./skladniaCzas"
-import { adverbsData } from './przyslowki';
-import { wordFormationData } from './slowotworstwo';
-import { modalVerbsData } from './modalne';
-import { articlesData } from './przedimki';
-import { adverbialClausesData } from './zdOkolicznosciowe';
-import { passiveVoiceTheoryData } from './stronaBierna';
-import { conditionalsData } from './trybWarunkowe';
-import { wishesAndPreferencesData } from './zyczenia';
-import { inversionAndEmphasisData } from './inwersja';
-import { possessivesData } from './formyDzierzawcze';
-import { pronounsMasterData } from './zaimki';
-import { relativeClausesData } from './zdaniaPrzudawkowe';
+import { tenses } from "./tenses";
+import { przyimki } from "./przyimki";
+import { thereIsItIsData } from "./thereIs";
+import { przymiotniki } from "./przymiotniki";
+import { reportedSpeechData } from "./mowaZalezna";
+import { verbPatternsData } from "./skladniaCzas";
+import { adverbsData } from "./przyslowki";
+import { wordFormationData } from "./slowotworstwo";
+import { modalVerbsData } from "./modalne";
+import { articlesData } from "./przedimki";
+import { adverbialClausesData } from "./zdOkolicznosciowe";
+import { passiveVoiceTheoryData } from "./stronaBierna";
+import { conditionalsData } from "./trybWarunkowe";
+import { wishesAndPreferencesData } from "./zyczenia";
+import { inversionAndEmphasisData } from "./inwersja";
+import { possessivesData } from "./formyDzierzawcze";
+import { pronounsMasterData } from "./zaimki";
+import { relativeClausesData } from "./zdaniaPrzudawkowe";
 export const grammarData = {
   "Czasy gramatyczne": tenses,
- 
+
   "Przyimki i Czasowniki złożone": przyimki,
   "There is / It is": thereIsItIsData,
-  "Przymiotniki" : przymiotniki,
+  Przymiotniki: przymiotniki,
   "Mowa zalezna": reportedSpeechData,
   "Składnia czasowników": verbPatternsData,
-  "Przysłówki": adverbsData,
-  "Słowotwórstwo": wordFormationData,
+  Przysłówki: adverbsData,
+  Słowotwórstwo: wordFormationData,
   "Czasowniki modalne": modalVerbsData,
-  "Przedimki": articlesData,
+  Przedimki: articlesData,
   "Zdania Okolicznościowe": adverbialClausesData,
   "Strona bierna": passiveVoiceTheoryData,
   "Tryby warunkowe": conditionalsData,
   "Zdania wyrażające życzenia": wishesAndPreferencesData,
   "Inwersja i wyrażenia empatyczne": inversionAndEmphasisData,
   "Formy dzierżawcze": possessivesData,
-  "Zaimki": pronounsMasterData,
+  Zaimki: pronounsMasterData,
   "Zdania przydawkowe": relativeClausesData,
 };
+
